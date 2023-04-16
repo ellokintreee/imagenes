2097023941b67ebe05c50e2c40773b9d50374f72
lokintro <danielakarinaceballes@gmail.com>
2023-04-16 06:21:02 -0300

    ahora cambia la imagen al azar

--- index.js
+++ index.js
@@ -4,22 +4,17 @@ function cambiarImagen() {
         let imagen = document.querySelector("img");
         imagen.getAttribute("src");
         imagen.setAttribute("src", `${changeLink()}`)
-        console.log(imagen)    
+      
         
 
 }
 /*me da un codigo diferente del array*/
 function changeLink(enlace){
-  //  let random = Math.floor(Math.random()*17);
+    let random = Math.floor(Math.random()*17);
+  var array = [
     
-  let element = "";
-  var suma = 0;
-  for (let i = 0; i < 17; i++) {
-      suma++;
-      
-      
-    }
-    var array = [
+
+
         "https://images.unsplash.com/photo-1680967230555-a803660575dc?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1026&q=80",
         "https://plus.unsplash.com/premium_photo-1674599003901-7794de153bbe?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=764&q=80",
         "https://images.unsplash.com/photo-1680562725444-5a9aaa12525b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
@@ -38,10 +33,7 @@ function changeLink(enlace){
       "https://images.unsplash.com/photo-1680798790340-e1ec34621d1a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=689&q=80",
       "https://images.unsplash.com/photo-1680868354675-34192a9baae7?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1476&q=80"
     ]
-    
-    element += array[suma];
- 
- return element
-}
+ return array[random]
+ }
 
 console.log(changeLink(     )) 
